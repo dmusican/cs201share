0deb20fcc89942b1c97b6d4dd7863cbc42bd2f8a
Dave Musicant (Carleton VM) <dmusican@carleton.edu>
2015-04-01 11:04:33 -0500

    class updates

--- CoolGame.java
+++ CoolGame.java
@@ -3,8 +3,9 @@ public class CoolGame
     public static void main(String[] args)
     {
         Die die1 = new Die();
+        System.out.println(die1.getNumSides());
         die1.roll();
         int value = die1.onTop();
-        System.out.println(value);
+        System.out.println("value = " + value);
     }
 }
